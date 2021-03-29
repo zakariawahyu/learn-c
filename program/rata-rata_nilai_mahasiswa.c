03fe8bf0fa3fe76a0aff783ebf91e23543197f8a
Zakaria Wahyu <zakarianur6@gmail.com>
2021-03-29 20:12:28 +0700

    Menyimpan nilai min dan max ke dalam array

--- program/rata-rata_nilai_mahasiswa.c
+++ program/rata-rata_nilai_mahasiswa.c
@@ -3,13 +3,17 @@
 void main(){
     printf("----- Program Menghitung Nilai Rata-Rata X Mahasiswa ----------\n");
 
-    int x_mahasiswa, nilai, total, average, lulus = 0, tidaklulus = 0;
+    int x_mahasiswa, nilai, total, lulus = 0, tidaklulus = 0;
     int i = 1;
-    int min, max;
+    int min, max, indexMin, indexMax;
+    float average;
   
 
     printf("Masukkan jumlah mahasiswa : ");
     scanf("%d", &x_mahasiswa);
+
+    // deklarasi panjang array sesuai inputan user
+    int arrayNilai[x_mahasiswa];
 
     while (i <= x_mahasiswa)
     {
@@ -17,11 +21,16 @@ void main(){
         scanf("%d", &nilai);
         total = total + nilai;
 
-        // Mengetahui nilai minimum dan max
-        if (nilai > max){
-            max = nilai;
-        } else if (nilai < min) {
-            min = nilai;
+        // meyimpan nilai ke dalam array
+        arrayNilai[i] = nilai;
+
+        // Mengetahui nilai minimum dan max di index array 
+        if (arrayNilai[i] > max){
+            max = arrayNilai[i];
+            indexMax = i;
+        } else if (arrayNilai[i] < min) {
+            min = arrayNilai[i];
+            indexMin = i;
         }
 
         // Mengetahui lulus atau tidaknya
@@ -34,11 +43,14 @@ void main(){
         i++;
     }
 
+    // menghitung rata-rata
     average = total/x_mahasiswa;
+
+    // print
     printf("Jumlah nilai = %d \n", total);
-    printf("Nilai rata-rata mahasiswa : %d \n", average);
-    printf("Nilai minimum : %d \n", min);
-    printf("Nilai max : %d \n", max);
+    printf("Nilai rata-rata mahasiswa : %.2f \n", average);
+    printf("Nilai minimum : %d pada index ke %d \n", min, indexMin);
+    printf("Nilai max : %d pada index ke %d \n", max, indexMax);
     printf("Jumlah mahasiswa lulus : %d \n", lulus);
     printf("Jumlah mahasiswa tidak lulus : %d \n", tidaklulus);
     printf("----------------- Selesai ----------------------------");
